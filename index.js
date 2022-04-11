43359610a064fc12d34624242182a3c6d36e4678
JoelHauser <joel.hauser@gmail.com>
2022-04-11 15:56:27 -0400

    WIP

---
+++ index.js
@@ -0,0 +1,6 @@
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+const inquirer = require('inquirer');
+const fs = require('fs');
